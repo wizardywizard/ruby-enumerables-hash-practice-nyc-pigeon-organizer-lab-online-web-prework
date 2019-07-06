3d211571e29c9c1e101fcc191ce860597974ac57
David Little <littledavid175@gmail.com>
2019-07-06 20:37:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |next_value, names|
+      names.each do |name|
+        
+       if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        
+        new_hash[name][key] << next_value.to_s
+        # binding.pry
+       end
+     end
+  end
+  new_hash
 end
+#make a new hash using the birds name as keys and color, gender, and lives as values
